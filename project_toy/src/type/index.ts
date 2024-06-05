277283f04a6a8988a9cd58de1590f209906b709b
bishoe01 <bishoe01@gmail.com>
2024-06-05 23:54:04 +0900

    [FIX] 타입 변수명 및 Write 타입 수정 id : number -> string , name 삭제

--- project_toy/src/type/index.ts
+++ project_toy/src/type/index.ts
@@ -1,14 +1,14 @@
 
-// 유저 정보
-type Writer ={
-    id: number;
-    name: string;
-}
+// 타입 정의
+export type Writer = {
+  id: string;
+};
 
-// 게시글 정보
-type WritePage ={
-    title: string;
-    content: string;
-    writer: Writer;
-}
 
+export type Posts = {
+  post_id: number;
+  title: string;
+  content: string;
+  date: string;  // DATE타입에서 -> string타입으로 변경(ISO 포맷)
+  writer: Writer;
+};
