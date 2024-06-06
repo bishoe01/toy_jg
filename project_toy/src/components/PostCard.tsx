81b7301a96a9a4d12416bc57a07f7e1f62afc2c6
bishoe01 <bishoe01@gmail.com>
2024-06-06 17:15:26 +0900

    [UPDATE] 게시판 글 목록 UI 변경

--- project_toy/src/components/PostCard.tsx
+++ project_toy/src/components/PostCard.tsx
@@ -1,7 +1,7 @@
 import { useDeletePost } from "@/hooks/usePostsActions";
-import { FaRegTrashAlt } from "react-icons/fa";
 import { confirm_alert } from "@/utils/confirmDelete";
 import { useRouter } from "next/navigation";
+import { LiaComment, LiaHeartSolid, LiaThumbtackSolid } from "react-icons/lia";
 import { Posts } from "@/type";
 
 interface PostCardProps {
@@ -15,24 +15,56 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
   const handleDeleteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     confirm_alert("정말 삭제하시겠습니까?", "삭제", "#FF2200", () => {
-      deletePost(post.post_id);
+      deletePost(post._id as string);
     });
   };
 
   return (
     <div
-      onClick={() => router.push(`/posts/${post.post_id}`)}
-      className="rounded-md border p-4 relative cursor-pointer hover:scale-105 hover:border-green-500 hover:shadow-lg transition-transform duration-300 ease-in-out"
+      onClick={() => router.push(`/posts/${post._id}`)}
+      className="bg-primary rounded-2xl p-4 relative cursor-pointer hover:brightness-125 transition-transform duration-300 ease-in-out shadow-md" // 그림자 추가
     >
-      <h2 className="text-lg font-bold">{post.title}</h2>
-      <p>{post.content}</p>
+      <div className="flex items-center justify-between mb-2">
+        <span className="text-sm font-medium text-whiter">
+          {post.writer.id}
+        </span>
+        <button
+          className="p-2 bg-red-600 rounded-full hover:bg-red-700"
+          onClick={handleDeleteClick}
+        >
+          <LiaThumbtackSolid className="text-white text-lg" />
+        </button>
+      </div>
 
-      <button
-        className="absolute top-2 right-2 z-10"
-        onClick={handleDeleteClick}
-      >
-        <FaRegTrashAlt className="hover:text-red-500 rounded-md text-gray-700 text-xl" />
-      </button>
+      <h2 className="text-xl font-bold text-white mb-2 line-clamp-2">
+        {post.title}
+      </h2>
+
+      <div className="relative overflow-hidden group">
+        <p
+          className="text-gray-secondary bg-secondary p-3 rounded-md text-sm line-clamp-3 overflow-hidden"
+          style={{
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 3,
+          }}
+        >
+          {post.content}
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque eum
+          saepe, nostrum molestias voluptatibus sapiente necessitatibus ullam!
+          Consequatur, placeat at!
+        </p>
+        <div
+          className="absolute inset-0 bg-gradient-to-t from-primary
+         to-transparent pointer-events-none group-hover:hidden"
+        ></div>{" "}
+      </div>
+
+      <div className="flex items-center mt-2 text-gray-300">
+        <LiaHeartSolid className="mr-1" />
+        0
+        <LiaComment className="ml-4 mr-1" />0
+      </div>
     </div>
   );
 };
--- project_toy/src/components/PostList.tsx
+++ project_toy/src/components/PostList.tsx
@@ -1,15 +1,39 @@
 "use client";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { postListState } from "@/store/posts";
 import PostCard from "./PostCard";
+import { useEffect } from "react";
+import axios from "axios";
 
 const PostList: React.FC = () => {
-  const postList = useRecoilValue(postListState);
+  const [postList, setPostList] = useRecoilState(postListState);
+  useEffect(() => {
+    axios.get("/api/posts").then((res) => {
+      setPostList(res.data);
+    });
+  }, []);
 
   return (
-    <div className="w-full grid grid-cols-3 gap-4">
+    <div className=" w-full flex flex-col gap-4">
+      <button
+        className="bg-secondary text-whiter px-2 py-1 rounded-lg mt-2"
+        onClick={(e) => {
+          e.stopPropagation();
+          // axios요청 to add post
+          axios.post("/api/posts", {
+            title: "한글 제목",
+            content: "한글 내용, 그리고 그렇게 된다",
+            date: new Date().toISOString(),
+            writer: {
+              id: "한글맨",
+            },
+          });
+        }}
+      >
+        ADD
+      </button>
       {postList.map((post) => (
-        <PostCard key={post.post_id} post={post} />
+        <PostCard key={post._id} post={post} />
       ))}
     </div>
   );
